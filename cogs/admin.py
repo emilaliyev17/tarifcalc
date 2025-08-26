c799ce1db830713c070489d4af08cd1b75ea6635
Emil Aliyev <emil.aliyev@Emils-MacBook-Pro.local>
2025-08-25 17:25:33 -0700

    Register models in Django Admin

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import HSUSCode, SKU, Container, Invoice, InvoiceLine, CostPool, AllocatedCost
+ 
 # Register your models here.
+admin.site.register(HSUSCode)
+admin.site.register(SKU)
+admin.site.register(Container)
+admin.site.register(Invoice)
+admin.site.register(InvoiceLine)
+admin.site.register(CostPool)
+admin.site.register(AllocatedCost)
