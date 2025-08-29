10fd9c8fd657e0e12d5e81ca8b1b64159a142d06
Emil Aliyev <emil.aliyev@Emils-MacBook-Pro.local>
2025-08-28 17:56:27 -0700

    feat(tariff): redirect root to tariff app and move cogs urls to /legacy/

--- core/urls.py
+++ core/urls.py
@@ -12,6 +12,7 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
+from django.views.generic import RedirectView
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
@@ -22,7 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', include('cogs.urls')),
+    path('', RedirectView.as_view(pattern_name='tariff_shipment_entry', permanent=False)),
+path('legacy/', include('cogs.urls')),
     path('api/', include('consolidation_app.urls')),
     path("tariff/", include("tariff.urls")),
 ]
